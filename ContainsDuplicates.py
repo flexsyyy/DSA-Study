ea78bbd6e28a66e6693c31105a1e7459a15af02e
flexsyyy <118766190+flexsyyy@users.noreply.github.com>
2026-05-05 22:59:29 +0530

    update duplicate
    Co-Authored-By: Mohammed Abrar Akram Farooque <abrarfarooquelearns@gmail.com>

--- ContainsDuplicates.py
+++ ContainsDuplicates.py
@@ -1,8 +1,7 @@
 """"
-Contains Duplicate
-Solved 
+Contains Duplicate problem on leetcode, we can solve this problem using two approaches, one is brute force method and another is using hashmap(dictionary) to store the values and their indices.
 Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
-
+Solved 
 Example 1:
 Input: nums = [1, 2, 3, 3]
 Output: true
